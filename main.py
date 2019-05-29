30bc75af076aba32b484b2ed023eadbcb87397d0
q359639394 <51103922+q359639394@users.noreply.github.com>
2019-05-29 08:40:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import ujson
+import time
+import MQTT
+import machine
+
+print('***************adding from pythonA')
+print('**************this is a demo ******************')
+print('**************master adding ******************')
+print('**************adding now*******************')
+print('**************adding from master branch')
+print('**************adding from pythonD branch*************')
+
+
+
